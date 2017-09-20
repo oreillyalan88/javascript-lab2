c33ac4285a15701bb8a969c5a2751add792a32d8
Alan O' Reilly <oreillyalan88@gmail.com>
2017-09-20 21:52:49 +0000

    addPreviousOwner function

--- functions.js
+++ functions.js
@@ -17,4 +17,20 @@ aCar.color = { exterior : 'red',
                interior : { texture : 'leather', 
                             shade : 'cream' }
               } ;
-             
+
+ var addPreviousOwner = function(car, newName, newAddress) {
+           var o = { name : newName, address : newAddress } ;
+           car.previous_owners.push(o) ;
+           
+           if (car.previous_owners.length-1 >= 3){
+           car.previous_owners.shift(0) ;}
+     } ;
+
+
+addPreviousOwner(aCar,'Jim Nugent','3 Lower Road') ;
+addPreviousOwner(aCar,'Rachel Fleming','4 Upper Road') ;
+
+
+     console.log(aCar.previous_owners[2].name) ;
+     console.log(aCar.previous_owners[0].name) ;
+	// console.log(aCar);
